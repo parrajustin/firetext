e7b0853b0780dc2c494625e150106ae0862360d8
parrajustin <parra.justin.main@gmail.com>
2017-10-13 12:11:39 -0600

    Non-functioning build in electron for #353

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": "idiomatic",
+  "rules": {
+    "one-var": ["error", "never"],
+    "semi": ["error", "always"],
+    "no-console": ["error", { allow: ["warn", "debug", "error", "info"] }]
+  }
+};
